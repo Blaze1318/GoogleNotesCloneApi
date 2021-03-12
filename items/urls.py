3273dbd871ee85aaf35e686e2271c1438f70d4f2
Blaze1318 <39748904+Blaze1318@users.noreply.github.com>
2021-03-11 23:40:45 -0400

    no changes

--- items/urls.py
+++ items/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path,include
 from . import views
 from rest_framework import routers
+from rest_framework.urlpatterns import format_suffix_patterns
 
 router = routers.DefaultRouter()
 router.register('items',views.ItemsView)
 
 urlpatterns = [
-    path('',include(router.urls))
+    path('',include(router.urls)),
 ]
+
